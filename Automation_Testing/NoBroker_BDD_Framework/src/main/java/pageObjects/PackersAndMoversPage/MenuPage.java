9d93fc0f7d9a9595c17504dfd6e3f637a85b062e
sikta_das <siktadas1133@gmail.com>
2026-03-25 22:47:50 +0530

    Implemented Page Object Model, created BDD feature file, and added initial step definitions for Packers & Movers module

---
+++ Automation_Testing/NoBroker_BDD_Framework/src/main/java/pageObjects/PackersAndMoversPage/MenuPage.java
@@ -0,0 +1,35 @@
+package pageObjects.PackersAndMoversPage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MenuPage {
+
+    WebDriver driver;
+
+    public MenuPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    
+    @FindBy(id="main-menu")
+    private WebElement menu;
+
+    @FindBy(xpath="//div[@id='main-menu']//div//a[text()='Packers and Movers']")
+    private WebElement option;
+    
+    public void MenuClick() {
+    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable(menu)).click();
+    }
+    
+    public void selectOption() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(option)).click();
+    }}
