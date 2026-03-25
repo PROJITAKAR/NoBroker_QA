3cff9234480b160017c4187a672e5b0a150072c0
sikta_das <siktadas1133@gmail.com>
2026-03-25 23:50:17 +0530

    Implemented Page Object Model, created BDD feature file, added initial step definitions for Packers & Movers module and added the xml file

--- Automation_Testing/NoBroker_BDD_Framework/src/test/java/stepDefinitions/PackersAndMovers_StepDefinition.java
+++ Automation_Testing/NoBroker_BDD_Framework/src/test/java/stepDefinitions/PackersAndMovers_StepDefinition.java
@@ -1,27 +1,68 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
-import pageObjects.PackersAndMoversPage;
+import io.cucumber.java.en.*;
+
+import pageObjects.PackersAndMoversPage.MenuPage;
+import pageObjects.PackersAndMoversPage.SlotBookingPage;
+import pageObjects.PackersAndMoversPage.LocationPage;
+import pageObjects.PackersAndMoversPage.InventoryPage;
 import utils.DriverFactory;
 
 public class PackersAndMovers_StepDefinition {
 
     WebDriver driver = DriverFactory.getDriver();
-    PackersAndMoversPage page = new PackersAndMoversPage(driver);
 
-    @Given("user is on Packers and Movers page")
-    public void user_on_packers_page() {
-        page.navigateToPackersPage();
+    MenuPage menuPage = new MenuPage(driver);
+    LocationPage locationPage = new LocationPage(driver);
+    InventoryPage inventoryPage = new InventoryPage(driver);
+    SlotBookingPage slotBookingPage = new SlotBookingPage(driver);
+    // Step 1
+    @Given("user navigates to Packers and Movers Page")
+    public void user_navigates_to_packers_and_movers_page() {
+        //driver.get("https://www.nobroker.in/");
+        menuPage.MenuClick();
+        menuPage.selectOption();
     }
 
-    @Then("page should load successfully")
-    public void verify_page() {
-        String title = page.getPageTitle();
-        Assert.assertTrue(title.contains("Packers"));
+    // Step 2
+    @And("user selects a city, pickup and drop location")
+    public void user_selects_city_pickup_and_drop() throws InterruptedException {
+        locationPage.pickupLoc("Koramangala");
+        Thread.sleep(2000); 
+
+        locationPage.dropLoc("Cox Town");
+        Thread.sleep(2000);
     }
+
+    // ================= SCENARIO 1 =================
+
+    @When("user selects {string} option")
+    public void user_selects_shifting_type() {
+        locationPage.type(); 
+    }
+
+    // Step 4
+    @And("navigates to inventory Page")
+    public void navigates_to_inventory_page() {
+        locationPage.checkPrices();
+    }
+
+    // Step 5
+    @Then("user searches for item {string}")
+    public void user_searches_for_item(String item) throws InterruptedException {
+        inventoryPage.searchItem();
+        inventoryPage.dataEntry(item);
+        Thread.sleep(2000);
+    }
+
+    // Step 6
+    @And("no relevant results should be displayed")
+    public void no_relevant_results_should_be_displayed() {
+        String actualMessage = inventoryPage.getNoItemMessage();
+        Assert.assertEquals(actualMessage, "No items found");
+    }
+
 }
