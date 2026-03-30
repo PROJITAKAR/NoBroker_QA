a33e6787d07f59c85d7c9a68fcf5fe7d5ee3a465
Suvam007 <121725387+Suvam007@users.noreply.github.com>
2026-03-30 19:29:34 +0530

    Rest Assured commit changes

--- API_Testing/Restful_Booker_RestAssured/src/test/java/tests/CreateBookingTest.java
+++ API_Testing/Restful_Booker_RestAssured/src/test/java/tests/CreateBookingTest.java
@@ -1,5 +1,31 @@
 package tests;
 
-public class CreateBookingTest {
+import base.BaseTest;
+import endpoints.BookerEndpoints;
+import payloads.Booking;
+import payloads.BookingDates;
+import utils.TestDataBuilder;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class CreateBookingTest extends BaseTest {
+
+    @Test
+    public void testCreateBooking() {
+
+
+        Response response = BookerEndpoints.createBooking(request, TestDataBuilder.createBookingPayload());
+        id = response.path("bookingid");
+        //System.out.println(id);
+        
+        firstname = response.path("booking.firstname");
+        lastname = response.path("booking.lastname");
+        checkin = response.path("booking.bookingdates.checkin");
+        checkout = response.path("booking.bookingdates.checkout");
+        
+        System.out.println(response.asPrettyString());
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
 }
